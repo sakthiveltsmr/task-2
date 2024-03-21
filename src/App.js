39729ab046f7e622e4d76992318c80a209717abc
sakthiveltsmr <sakthiveltsmr@gmail.com>
2024-03-21 19:47:33 +0530

    fixed:checkbox task

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  let sports=["playing cricket","playing carom","palying foodball"];
 
+function App() {
+  let activity=["playing cricket","playing carom","palying foodball"];
+  let modify_activity=activity.map((item,index)=>{
+    return {activity:item,checked:false}
+  });
+  const[sports,setSports]=useState(modify_activity);
+
+ const RemoveItem=(index)=>{
+  let data=sports.filter((item,i)=>i!==index);
+  setSports(data)
+  }
+
+  const SelectItem=(index)=>{
+    let data=sports.map((item,i)=>{
+      if(i===index){
+        item.checked=!item.checked
+      }
+      return item
+    })
+    console.log(data)
+    setSports(data)
+  }
   return (
-    <div className="App">
-      {sports.map((item)=>{
+    <div className="">
+      {sports.length!==0?
+      <>
+      {sports.map((item,index)=>{
         return(
           <ul>
-            <li>{item}</li>
-            <button>Delete</button>
+            <li><span><input type='checkbox' value={item.checked} onClick={()=>SelectItem(index)}/></span><span>{item.activity}</span>{item.checked&&<button onClick={()=>RemoveItem(index)}>Delete</button>}</li>
           </ul>
         )
-      })}
+      })}</>:<></>}
     </div>
   );
 }
